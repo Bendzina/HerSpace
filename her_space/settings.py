4659ab3993882e02128cfbd2595734aa493d5929
Bendzina <165944559+Bendzina@users.noreply.github.com>
2025-07-16 22:34:07 +0400

    init

--- her_space/settings.py
+++ her_space/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wubh)^lvqb&q1cxvaw=_p2^tg&s05_y-9&p3n_7*%0^*p0*h*u'
+SECRET_KEY = 'django-insecure-^d!i73c1att*5*t9x#y0r0ko65f@s2znjl$)d1@tm#uzm*bdue'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
